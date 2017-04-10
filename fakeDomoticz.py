75007e59b8ce7202d796bba056e58dd6bcd30c05
gerard33 <gerard33@users.noreply.github.com>
2017-04-11 00:10:01 +0200

    Create fakeDomoticz.py

---
+++ fakeDomoticz.py
@@ -0,0 +1,17 @@
+#
+#   Sony Bravia - Domoticz Python plugin
+#
+#   With thanks to Frank Fesevur, 2017
+#
+#   Very simple module to make local testing easier
+#   It "emulates" Domoticz.Log(), Domoticz.Error and Domoticz.Debug()
+#
+
+def Log(s):
+    print(s)
+
+def Error(s):
+    print(s)
+
+def Debug(s):
+    print(s)
